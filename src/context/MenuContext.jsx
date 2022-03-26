802b15b0b8438fe58700cc28caa9199b7587dc38
mostafa <mostafamnbvcxz5@gmail.com>
2022-03-26 10:44:51 +0430

    add context

---
+++ src/context/MenuContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useState } from 'react';
+
+const MenuContext = createContext();
+
+const MenuContextProvider = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <MenuContext.Provider
+      value={{
+        isOpen,
+        setIsOpen,
+      }}
+    >
+      {children};
+    </MenuContext.Provider>
+  );
+};
+
+export default MenuContext;
+export { MenuContextProvider };
