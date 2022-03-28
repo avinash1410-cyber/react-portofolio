a7c3641824456c2c2f581ca994d4665cfbeef964
Mostafa Kheibary <mostafa.kheibary@gmail.com>
2022-03-28 10:51:02 +0500

    add icon bar

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,29 +1,29 @@
-import Container from '../../components/shared/Container/Container';
-import { Content } from './ContactStyle';
-import Text from '../../components/shared/Text/Text';
-// import Icons from '../../components/layouts/SocialBar/SocialBar';
+import Container from "../../components/shared/Container/Container";
+import { Content } from "./ContactStyle";
+import Text from "../../components/shared/Text/Text";
+import Icons from "../../components/layouts/IconsBar/SocialBar";
 
 const Contact = () => {
   return (
     <Container>
       <Content
-        key='page4'
-        initial={{ y: '5rem', opacity: 0 }}
+        key="page4"
+        initial={{ y: "5rem", opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
-        exit={{ y: '-5rem', opacity: 0 }}
+        exit={{ y: "-5rem", opacity: 0 }}
       >
-        <Text type='h4' size={2.8} weight='500'>
-          Contact with Me{' '}
+        <Text type="h4" size={2.8} weight="500">
+          Contact with Me{" "}
         </Text>
         <ul>
-          <Text type='li' size={1.8} weight={300}>
+          <Text type="li" size={1.8} weight={300}>
             phone Number : 09107584206
           </Text>
-          <Text type='li' size={1.8} weight={300}>
+          <Text type="li" size={1.8} weight={300}>
             Email : mostafa.kheibary@gmail.com
           </Text>
         </ul>
-        {/* <Icons /> */}
+        <Icons />
       </Content>
     </Container>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,28 +1,28 @@
-import { Image, Avatar, Content } from './HomeStyle';
-import Text from '../../components/shared/Text/Text';
-import avatar from '../../image/1.png';
-import Container from '../../components/shared/Container/Container';
-// import Icons from '../../components/layouts/IconsBar/SocialBar';
+import { Image, Avatar, Content } from "./HomeStyle";
+import Text from "../../components/shared/Text/Text";
+import avatar from "../../image/1.png";
+import Container from "../../components/shared/Container/Container";
+import Icons from "../../components/layouts/IconsBar/SocialBar";
 
 const Home = () => {
   return (
     <Container>
       <Content
-        key='page1'
-        initial={{ y: '5rem', opacity: 0 }}
+        key="page1"
+        initial={{ y: "5rem", opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
-        exit={{ y: '-5rem', opacity: 0 }}
+        exit={{ y: "-5rem", opacity: 0 }}
       >
         <Avatar>
-          <Image src={avatar} alt='' />
+          <Image src={avatar} alt="" />
         </Avatar>
-        <Text type='h1' size={3.5}>
-          Hi im Mostafa <span className='shake'>👋🏻</span>
+        <Text type="h1" size={3.5}>
+          Hi im Mostafa <span role={"img"}>👋🏻</span>
         </Text>
-        <Text weight={300} type='h2' size={2}>
+        <Text weight={300} type="h2" size={2}>
           Front-end developer
         </Text>
-        {/* <Icons /> */}
+        <Icons />
       </Content>
     </Container>
   );
