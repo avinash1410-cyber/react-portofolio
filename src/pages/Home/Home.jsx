b9d582b77b6f3d2cbbc3f0bb98a6f366e7f2a89f
mostafa kheibary <mostafa.kheibary@gmail.com>
2022-05-07 18:46:28 +0430

    make seo bether and comppres image size

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,25 @@
-import { Image, Avatar, Content } from "./HomeStyle";
-import Text from "../../components/shared/Text/Text";
-import avatar from '../../image/avatar.png';
-import Container from "../../components/shared/Container/Container";
-import Icons from "../../components/layouts/IconsBar/SocialBar";
+import { Image, Avatar, Content } from './HomeStyle';
+import Text from '../../components/shared/Text/Text';
+import avatar from '../../image/avatar.webp';
+import Container from '../../components/shared/Container/Container';
+import Icons from '../../components/layouts/IconsBar/SocialBar';
 
 const Home = () => {
   return (
     <Container>
       <Content
-        key="page1"
-        initial={{ y: "5rem", opacity: 0 }}
+        key='page1'
+        initial={{ y: '5rem', opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
-        exit={{ y: "-5rem", opacity: 0 }}
+        exit={{ y: '-5rem', opacity: 0 }}
       >
         <Avatar>
-          <Image src={avatar} alt="" />
+          <Image src={avatar} alt='mostafa kheibary picture' />
         </Avatar>
-        <Text type="h1" size={3.5}>
-          Hi im Mostafa <span role={"img"}>👋🏻</span>
+        <Text type='h1' size={3.5}>
+          Hi im Mostafa <span role={'img'}>👋🏻</span>
         </Text>
-        <Text weight={300} type="h2" size={2}>
+        <Text weight={300} type='h2' size={2}>
           Front-end developer
         </Text>
         <Icons />
